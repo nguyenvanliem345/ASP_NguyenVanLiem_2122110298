a1d26af9ee0549a4a49b7eefc699a5e98e878802
nguyenvanliem345 <nguyenvanliem309@gmail.com>
2025-03-28 23:06:38 +0700

    code asp hoc buoi 2

---
+++ Nguyenvanliem_2122110298/Data/AddDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nguyenvanliem_2122110298.Model;
+
+namespace Nguyenvanliem_2122110298.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Nguyenvanliem_2122110298/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Nguyenvanliem_2122110298.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
